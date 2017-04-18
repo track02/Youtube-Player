599134bd78a34e3ed63b012088542ecb1521fea8
track02 <a.clarke@runbox.com>
2017-04-18 14:04:56 +0100

    Structuring/Commenting for next steps

--- Scripts/Skip.js
+++ Scripts/Skip.js
@@ -1,12 +1,25 @@
-console.log("Called!");
-
 function enableHandler(event){
-	console.log("Clicked");
+	console.log("Clicked Enable");
 	mins = document.getElementById("min");
 	console.log(mins.value);
 	secs = document.getElementById("sec");
 	console.log(secs.value);
+	
+	//Write to storage - Active_State = True
+		
+}
+
+
+function disableHandler(event){
+	
+	console.log("Clicked Disable");
+	
+	//Write to storage - Active_State = False
+	
 }
 
 enable_button = document.getElementById("Enable");
 enable_button.onclick = enableHandler;
+
+disable_button = document.getElementById("Enable");
+disable_button.onclick = disableHandler;
--- Scripts/skipContent.js
+++ Scripts/skipContent.js
@@ -1,3 +1,26 @@
 console.log("Youtube page - script loaded");
-alert("Testing");
-window.location.href = (window.location.href + "#t=5m");
+
+
+
+
+//Read from storage - Active_State
+
+
+//If Active_State
+
+	//Determine if Timestamp present - Timestamp_Present
+
+	//If ! Timestamp_Present
+
+	//Read from storage - Minutes
+	//Read from storage - Seconds
+	
+	//Build Timestamp
+	
+	//Set Timestamp
+
+	window.location.href = (window.location.href + "#t=5m");
+
+	//End_If
+	
+//End_if
