cc53578d25f830b19ad545c1a4f1e24e4dc6535f
Alex Clarke <a.clarke@runbox.com>
2017-08-18 09:05:19 +0100

    Formatting / Validation for Timestamps

--- Scripts/Skip.js
+++ Scripts/Skip.js
@@ -1,13 +1,10 @@
-(function(){
-	
+
 		mins = document.getElementById("min");
 		secs = document.getElementById("sec");
 		disable = document.getElementById("Disable");
 		enable = document.getElementById("Enable");
 
 	function enableHandler(event){
-		mins = document.getElementById("min");
-		secs = document.getElementById("sec");
 
 		console.log("Clicked Enable");
 		console.log(mins.value);
@@ -17,7 +14,7 @@
 		browser.storage.local.set({activeState: true, minutes: mins.value, seconds: secs.value});
 		
 		//Toggle button visibilty
-		toggleButtons();			
+		toggleButtons();					
 	}
 
 	function disableHandler(event){
@@ -25,7 +22,7 @@
 		console.log("Clicked Disable");
 		
 		//Write to storage - Active_State = False
-		browser.storage.local.set({data: {activeState: false}});
+		browser.storage.local.set({activeState: false, minutes: mins.value, seconds: secs.value});
 		
 		//Toggle button visibility
 		toggleButtons();		
@@ -38,9 +35,16 @@
 	}
 	
 	function onInputHandler(event){
-
 		browser.storage.local.set({activeState: true, minutes: mins.value, seconds: secs.value});
-
+	}
+	
+	function isEmpty(obj){
+		return (Object.getOwnPropertyNames(obj).length === 0);
+	}
+	
+	function onTabChange(tabId, changeInfo, tabInfo)
+	{		
+		console.log("Tab: " + tabId + " changed");
 	}
 	
 	mins.oninput = onInputHandler;
@@ -48,40 +52,28 @@
 	enable.onclick = enableHandler;
 	disable.onclick = disableHandler;
 	
-	
-	function isEmpty(obj){
-		return (Object.getOwnPropertyNames(obj).length === 0);
-}
 
+	browser.storage.local.get().then(	
+		function(data){	
+	
+			activeState = (data.activeState);
+			
+			if(activeState == true){
+				toggleButtons();
+			}			
 
-
-	browser.storage.local.get().then(
-	
-function(data){
-	console.log(data.length);
-	
-	
-	activeState = (data.activeState);
-	if(activeState == true){
-		toggleButtons();
-	}
-	mins.value = data.minutes;
-	secs.value = data.seconds;
-
-	if(isEmpty(data)){
-		mins.value = 0;
-		secs.value = 0;
-		
-		
-		browser.storage.local.set({activeState: false, minutes: mins.value, seconds: secs.value});
-		console.log("This should trigger when the data is empty");
-	}
-	
-	
-	
-	
-	});
-
+			if(isEmpty(data)){
+				mins.value = 0;
+				secs.value = 0;				
+				browser.storage.local.set({activeState: false, minutes: mins.value, seconds: secs.value});
+				console.log("This should trigger when the data is empty");
+			}
+			else
+			{
+				mins.value = data.minutes;
+				secs.value = data.seconds;
+			}
+		}
+	);
 	
 	console.log("Loaded!");	
-})();
