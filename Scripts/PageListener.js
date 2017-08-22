da23fcf5b3bd5c3f38fc1f23c5b2453847fb111b
Alex Clarke <a.clarke@runbox.com>
2017-08-22 10:37:11 +0100

    PageListener Added
    PageListener script injected onto youtube pages, listens for messages
    from extension popup

---
+++ Scripts/PageListener.js
@@ -0,0 +1,38 @@
+/*
+*
+* Listens for requests to access player settings
+* 					TO DO				
+*
+*/
+
+function play(){
+
+}
+
+function pause(){
+
+}
+
+function volumeUp(){
+
+}
+
+function volumeDown(){
+
+}
+
+function nextVideo(){
+
+}
+
+function previousVideo(){
+
+}
+
+console.log("Page listener loaded");
+
+browser.runtime.onMessage.addListener(request => {
+
+	console.log("Message Received");
+	
+});
--- Scripts/Skip.js
+++ Scripts/Skip.js
@@ -8,7 +8,9 @@
 
 				
 		//Write to storage - Active_State = True
-		browser.storage.local.set({activeState: true, minutes: mins.value, seconds: secs.value});
+		browser.storage.local.set({activeState: true, 
+								   minutes: mins.value, 
+								   seconds: secs.value});
 		
 		//Toggle button visibilty
 		toggleButtons();					
@@ -17,7 +19,9 @@
 	function disableHandler(event){		
 		
 		//Write to storage - Active_State = False
-		browser.storage.local.set({activeState: false, minutes: mins.value, seconds: secs.value});
+		browser.storage.local.set({activeState: false,
+								   minutes: mins.value, 
+								   seconds: secs.value});
 		
 		//Toggle button visibility
 		toggleButtons();		
@@ -30,7 +34,9 @@
 	}
 	
 	function onInputHandler(event){
-		browser.storage.local.set({activeState: true, minutes: mins.value, seconds: secs.value});
+		browser.storage.local.set({activeState: true,
+								   minutes: mins.value, 
+								   seconds: secs.value});
 	}
 	
 
@@ -56,3 +62,22 @@
 	enable.onclick = enableHandler;
 	disable.onclick = disableHandler;
 
+
+
+function msgTabs(tabs) {
+  for (let tab of tabs) {
+
+  	if(tab.url.indexOf("youtube") != -1 && tab.url.indexOf("watch?") != -1){
+  		browser.tabs.sendMessage(tab.id, {greeting: "Sending a message"});
+  	}  	
+  }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+
+
+var querying = browser.tabs.query({}); //Create a query to fetch all tags
+querying.then(msgTabs, onError); //If successful send a message to each tag
