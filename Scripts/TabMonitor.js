a48adc4cb3b32b43672bf86bbadf068d3fa4c22d
Alex <a.clarke@runbox.com>
2018-03-18 14:23:34 +0000

    Initial Hotkeys - ToDo Easier Key Bindings

--- Scripts/TabMonitor.js
+++ Scripts/TabMonitor.js
@@ -1,5 +1,11 @@
 //On Youtube page & video playing without timestamp
 
+var volumeVal = 0;
+var timeVal = 0;
+var currentTabId = -1;
+
+
+
 function tabUpdated(tabId, changeInfo, tabInfo){
 	
 	function checkUrl(){
@@ -17,3 +23,48 @@ function tabUpdated(tabId, changeInfo, tabInfo){
 }
 	
 browser.tabs.onUpdated.addListener(tabUpdated);
+
+
+function initialiseValues(data){
+
+	volumeVal = data.vslider;
+	timeVal = data.tslider;
+	currentTabId = data.currentT;
+}
+
+function sendCommand(cmd)
+{
+
+	browser.storage.local.get().then(data =>
+	{
+		initialiseValues(data);		
+		if (currentTabId != -1 && currentTabId != undefined){
+			console.log(timeVal);
+			browser.tabs.sendMessage(currentTabId, {command: cmd, parameter: timeVal});	
+		}
+	});
+}
+
+browser.commands.onCommand.addListener(function(command) {
+  if (command == "play-pause-hotkey") {
+	console.log("Play hk");
+	sendCommand("play");
+  }
+  if (command == "play-next-hotkey"){
+	console.log("Next hk");
+	sendCommand("next video");
+  }
+  if (command == "replay-hotkey"){
+	console.log("Replay hk");
+	sendCommand("restart");
+  }
+  if (command == "skip-fwd-hotkey"){
+	console.log("fwd hk");
+	sendCommand("time skip f");
+  }
+  if (command == "skip-back-hotkey"){
+	console.log("back hk");
+	sendCommand("time skip b");
+  }
+
+});
