b15c595f3e35fe99bfc4055de720cab25f73ab35
Alex Clarke <a.clarke@runbox.com>
2018-03-28 15:48:54 +0100

    Initial Tests
    Initial hotkey tests using key listener and message passing

--- Scripts/BrowserMonitor.js
+++ Scripts/BrowserMonitor.js
@@ -3,18 +3,22 @@ var volumeVal = 5;
 var timeVal = 5;
 var currentTabId = -1;
 
+// Initial tab search 
+
+function checkUrl(url){
+	return ((url.indexOf("youtube") !== -1) && (url.indexOf("watch?") !== -1))
+}	
+
+
 //On Youtube page & video playing without timestamp
 function tabUpdated(tabId, changeInfo, tabInfo){
-	
-	function checkUrl(){
-		return ((changeInfo.url.indexOf("youtube") !== -1) && (changeInfo.url.indexOf("watch?") !== -1))
-	}	
-	
+
 	//Check if a video is being watched
-	if (changeInfo.url && checkUrl() )
+	if (changeInfo.url && checkUrl(changeInfo.url) )
 	{
 		//Update current TabId for hotkeys for use a fallback
 		currentTabId = tabId;
+		console.log(currentTabId);
 		
 		//Send update requests - ToDo > Should only send if popup is active otherwise no receiving end to connection
 		browser.runtime.sendMessage({command: "update headings", id: tabId, newtitle: changeInfo.title});		
@@ -24,12 +28,16 @@ function tabUpdated(tabId, changeInfo, tabInfo){
 //Listen for tab updates using above func 
 browser.tabs.onUpdated.addListener(tabUpdated);
 
+//Listen for requests from key listener
+browser.runtime.onMessage.addListener(keyMessageListener);
+
 function initialiseValues(data){
 
 	// Read in stored tab - if it's valid then use it otherwise stick with default 
-	var tabReq = browser.tabs.get(data.currentT);
-	tabReq.then((validTab) => {currentTabId = (checkUrl(validTab.url)) ? validTab.id : currentTabId});
-	
+	if (data.currentT != undefined){
+		var tabReq = browser.tabs.get(data.currentT);
+		tabReq.then((validTab) => {currentTabId = (checkUrl(validTab.url)) ? validTab.id : currentTabId});
+	}
 	volumeVal = (data.vslider == undefined) ? volumeVal : data.vslider;
 	timeVal = (data.tslider == undefined) ? timeVal : data.tslider;
 }
@@ -46,25 +54,28 @@ function sendCommand(cmd)
 	});
 }
 
-browser.commands.onCommand.addListener(function(command) {
-  if (command == "play-pause-hotkey") {
-	console.log("Play hk");
-	sendCommand("play");
-  }
-  if (command == "play-next-hotkey"){
-	console.log("Next hk");
-	sendCommand("next video");
-  }
-  if (command == "replay-hotkey"){
-	console.log("Replay hk");
-	sendCommand("restart");
-  }
-  if (command == "skip-fwd-hotkey"){
-	console.log("fwd hk");
-	sendCommand("time skip f");
-  }
-  if (command == "skip-back-hotkey"){
-	console.log("back hk");
-	sendCommand("time skip b");
-  }
-});
+function keyMessageListener(message)
+{	
+	var command = message.hotKeyCommand	
+	
+	if (command == "play-pause-hotkey") {
+		console.log("receive play hk");
+		sendCommand("play");
+	}
+	if (command == "play-next-hotkey"){
+		console.log("receive Next hk");
+		sendCommand("next video");
+	}
+	if (command == "replay-hotkey"){
+		console.log("receive Replay hk");
+		sendCommand("restart");
+	}
+	if (command == "skip-fwd-hotkey"){
+		console.log("receive fwd hk");
+		sendCommand("time skip f");
+	}
+	if (command == "skip-back-hotkey"){
+		console.log("receive back hk");
+		sendCommand("time skip b");
+	}
+}
---
+++ Scripts/KeyListener.js
@@ -0,0 +1,50 @@
+// Default values for hotkey commands 
+var volumeVal = 5;
+var timeVal = 5;
+var currentTabId = -1;
+var playPauseKey = " "; //Space
+var playNextKey = "h";
+var playResetKey = "j";
+var timeFwdKey = "k";
+var timeBckKey = "l";
+
+console.log("Loaded listener!");
+
+document.addEventListener('keypress', (event) => {
+  const keyName = event.key;
+
+  if (event.altKey && event.ctrlKey) {	  
+  
+	console.log(`ALT + CTRL + ${keyName}`);	 
+
+	if (keyName == playPauseKey) {
+		console.log("Play hk");
+		sendMessage("play-pause-hotkey");
+	}
+	if (keyName == playNextKey){
+		console.log("Next hk");
+		sendMessage("play-next-hotkey");
+	}
+	if (keyName == playResetKey){
+		console.log("Replay hk");
+		sendMessage("replay-hotkey");
+	}
+	if (keyName == timeFwdKey){
+		console.log("fwd hk");
+		sendMessage("skip-fwd-hotkey");
+	}
+	if (keyName == timeBckKey){
+		console.log("back hk");
+		sendMessage("skip-back-hotkey");
+	}  
+  }
+});
+
+
+function sendMessage(cmd)
+{	
+	browser.runtime.sendMessage({"hotKeyCommand": cmd})
+	
+}
+
+
